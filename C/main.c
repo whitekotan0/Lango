ab7257a13a740fe1d1c86ff5181bd2dff09b16dd
whitekotan <gcat5770@gmail.com>
2026-01-17 01:47:29 +0200

    feat: read file and print into console

--- C/main.c
+++ C/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 
 
 
@@ -6,6 +10,33 @@
 int main() 
 {
 	
-	
+	char langoCode;
+	FILE* file = fopen("main.lango", "r");
+
+	if (file != NULL)
+	{
+		while ((langoCode = fgetc(file)) != EOF)
+		{
+			printf("%c", langoCode);
+
+		}
+	}
+
+	fclose(file);
+	return 0;
 }
 
+
+/*
+
+function sum(a, b)
+{
+
+return a + b;
+
+}
+
+
+
+
+*/
